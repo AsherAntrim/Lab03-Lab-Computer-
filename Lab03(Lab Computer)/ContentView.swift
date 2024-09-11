841c673801763c9e7a01e8da715685cc7a43f01a
Asher Antrim <aantrim246@cedarville.edu>
2024-09-11 13:42:49 -0400

    copied code from laptop to lab computer for submission

--- Lab03(Lab Computer)/ContentView.swift
+++ Lab03(Lab Computer)/ContentView.swift
@@ -7,13 +7,49 @@
 
 import SwiftUI
 
+private struct Resturant: Identifiable {
+    var name: String
+    var id: String { name }
+}
+
+private let restaurants: [Resturant] = [
+    Resturant(name: "McDonald's"),
+    Resturant(name: "Arby's"),
+    Resturant(name: "Wendy's"),
+    Resturant(name: "Pizza Hut"),
+    Resturant(name: "Culver's")
+]
+
+struct ResturantView: View {
+    var name: String
+    @State private var isFavorite: Bool = false
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25)
+                .fill(Color.blue)
+            HStack {
+                Text(name)
+                    .font(.title)
+                    .foregroundColor(.white)
+                Spacer()
+                Text(isFavorite ? "👍" : "👎")
+                    .font(.title)
+                    .onTapGesture {
+                        isFavorite.toggle()
+                    }
+            }
+            .padding()
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ForEach(restaurants) { restaurant in
+                ResturantView(name: restaurant.name)
+            }
         }
         .padding()
     }
