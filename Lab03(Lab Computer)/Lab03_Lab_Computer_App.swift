96427bc897658d51c1583934483f6a70023040ee
Asher Antrim <aantrim246@cedarville.edu>
2024-09-11 13:41:39 -0400

    Initial Commit

---
+++ Lab03(Lab Computer)/Lab03_Lab_Computer_App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab03_Lab_Computer_App.swift
+//  Lab03(Lab Computer)
+//
+//  Created by Asher Antrim on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab03_Lab_Computer_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
